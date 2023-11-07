f36ec4605680edb6e455b467bc295e4af6d02ca4
marco <longhkt10@gmail.com>
2023-11-07 18:33:53 +0700

    Add project files.

---
+++ Netflix/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Netflix.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Netflix.Controllers
+{
+    public class HomeController : Controller
+    {
+        XemPhimEntities database = new XemPhimEntities();
+        public ActionResult TrangChu()
+        {
+            using (var dbContext = new XemPhimEntities())
+            {
+                var items = dbContext.Phims.ToList();
+
+                return View(items);
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ChiTietPhim(int Id)
+        {
+            var Phim = database.Phims.FirstOrDefault(s => s.IdPhim == Id);
+            return View(Phim);
+        }
+        public ActionResult XemPhim(int Id)
+        {
+            var Phim = database.Phims.FirstOrDefault(s => s.IdPhim == Id);
+            return View(Phim);
+        }
+    }
+}
